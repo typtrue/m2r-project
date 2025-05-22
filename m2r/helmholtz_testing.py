0d1fc7dbbce15b13bb4e4abbbeae1c1e6fa946d2
Dan <149019458+typtrue@users.noreply.github.com>
2025-05-22 23:31:42 +0100

    discrete kirchoff-helmholtz

--- m2r/helmholtz_testing.py
+++ m2r/helmholtz_testing.py
@@ -1,10 +1,34 @@
-import scipy as sp
+import scipy as sci
 import numpy as np
-from typing import Iterable
+from typing import Sequence
+import sympy as sp
 
 
-class Wave:
-    def __init__(self, wavelength: float, pos: Iterable[float], dir: Iterable[float]) -> None:
-        self.wavelength = wavelength
-        self.pos = pos
-        self.dir = dir
+class HelmholtzSystem:
+    def __init__(self, wave_speed: float, source_strength: float, plate_length: float = 2, plate_center: tuple[float, float] = (0, 0)) -> None:
+        self.c = wave_speed
+        self.source_strength = source_strength
+        self.plate_length = plate_length
+        self.plate_center = plate_center
+        l = plate_center[0] - plate_length # noqa e741
+        r = plate_center[0] + plate_length
+        t = plate_center[1] + plate_length
+        b = plate_center[1] - plate_length
+        self.domain = (b, t, l, r)
+
+    def kirchoff_helmholtz(self, reciever: tuple[float, float], surface, n):
+        x, y = reciever
+        Q = self.source_strength
+        domain = self.domain
+        r_Q = y - self.domain[0]
+        v_S = 0
+
+        k = 1
+
+        dx = (domain[3] - domain[2]) / n
+        dy = (domain[1] - domain[0]) / n
+
+
+        I1 = Q * np.exp(1j * k * r_Q) / r_Q
+
+        # I2 = v_S * np.exp(
