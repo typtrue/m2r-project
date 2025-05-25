6c0aae709d1cfd81b471d98b225e2ca9cb276419
Dan <149019458+typtrue@users.noreply.github.com>
2025-05-26 00:32:36 +0100

    more testing

---
+++ m2r/funcs.py
@@ -0,0 +1,65 @@
+import numpy as np
+import matplotlib.pyplot as plt
+from matplotlib import cm
+from matplotlib.ticker import LinearLocator
+
+
+def hankel_estimate(alpha, x):
+    G = np.euler_gamma
+    PI = np.pi
+    if alpha == 0:
+        return 1 + 2.0j * (G + np.log(x/2)) / PI
+    elif alpha == 1:
+        return 2.0j / (PI * x) + 1.0j * x * (G - 0.5 + np.log(x/2)) / PI + x/2
+    raise NotImplementedError(f"alpha must be 0 or 1, got {alpha}")
+
+
+def trapezium_rule(n, x0, xn, f):
+    xs = np.linspace(x0, xn, n)
+    samples = np.array([f(x) for x in xs])
+    h = (xn - x0) / n
+    slice = np.array(samples[1:-1]) * 2
+    samples[1:-1] = slice
+    return 0.5*h*sum(samples)
+
+
+def G(r, r_0):
+    return hankel_estimate(0, 1 * np.linalg.norm(r - r_0)) / 4.0j
+
+
+def func(r_0):
+    return lambda x: 1.0j * 1 * G(np.array([x, 0]), r_0)
+
+
+def testpoint(r_0, n=10):
+    ikG = func(r_0)
+    return trapezium_rule(n, -1, 1, ikG)
+
+
+r_0 = np.array([0, 1])
+
+ikG = func(r_0)
+
+n = 25
+
+xvals = np.linspace(-5, 5, n)
+yvals = np.linspace(-5, 5, n)
+
+x, y = np.meshgrid(xvals, yvals)
+
+z_vals = np.array([testpoint(r).imag for r in np.column_stack((x.flatten(), y.flatten()))])
+
+print(z_vals.shape)
+
+z = np.reshape(z_vals, (-1, n))
+
+print(z.shape)
+
+fig, ax = plt.subplots(subplot_kw={"projection": "3d"})
+
+surf = ax.plot_surface(x, y, z, cmap=cm.coolwarm, linewidth=0, antialiased=False)
+
+ax.set_zlim(-0.5, 1)
+fig.colorbar(surf, shrink=0.5, aspect=5)
+
+plt.show()
--- m2r/helmholtz_testing.py
+++ m2r/helmholtz_testing.py
@@ -2,33 +2,20 @@ import scipy as sci
 import numpy as np
 from typing import Sequence
 import sympy as sp
+from funcs import hankel_estimate
 
 
 class HelmholtzSystem:
-    def __init__(self, wave_speed: float, source_strength: float, plate_length: float = 2, plate_center: tuple[float, float] = (0, 0)) -> None:
-        self.c = wave_speed
-        self.source_strength = source_strength
-        self.plate_length = plate_length
-        self.plate_center = plate_center
-        l = plate_center[0] - plate_length # noqa e741
-        r = plate_center[0] + plate_length
-        t = plate_center[1] + plate_length
-        b = plate_center[1] - plate_length
-        self.domain = (b, t, l, r)
+    def __init__(self, wave_no: float) -> None:
+        self.k = wave_no
 
-    def kirchoff_helmholtz(self, reciever: tuple[float, float], surface, n):
-        x, y = reciever
-        Q = self.source_strength
-        domain = self.domain
-        r_Q = y - self.domain[0]
-        v_S = 0
+        y = sp.symbols('y')
+        self.w_inc = np.exp(1.0j * wave_no * y)
 
-        k = 1
+        self.dw_scat = -1.0j * wave_no
 
-        dx = (domain[3] - domain[2]) / n
-        dy = (domain[1] - domain[0]) / n
-
-
-        I1 = Q * np.exp(1j * k * r_Q) / r_Q
-
-        # I2 = v_S * np.exp(
+    def G(self, x, y):
+        return hankel_estimate(0, self.k * np.abs(x - y)) / 4.0j
+    
+    def DG(self, x, y):
+        return self.k * hankel_estimate(1, self.k * np.abs(x - y)) / 4.0j
