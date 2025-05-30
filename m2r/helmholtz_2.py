b1da4471abdcd00a08bed033dd6bb0b47a495da7
Dan <danielhaywood23@gmail.com>
2025-05-30 12:14:25 +0100

    clean up code

--- m2r/helmholtz_2.py
+++ m2r/helmholtz_2.py
@@ -8,21 +8,16 @@ import numpy as np
 class HelmholtzSystem:
     def __init__(self, wave_no: float, bcs="D") -> None:
         self.k = wave_no
-
+        self.N = 10 * wave_no
         self.bcs = bcs
 
-        # TODO: Add impedence boundary conditions?
-
-        if bcs not in "DN":  # check if boundary conditions are dirichlet or neumann:
+        # check if boundary conditions are dirichlet or neumann
+        if bcs not in "DN":
             raise NotImplementedError
         
-
         # solve using BEM for phi, our weight function
-        self.c, self.B = self.init_problem()
-
-        self.coeffs = np.linalg.solve(self.B, self.c)
-
-        print(self.coeffs)
+        self.c, self.B = self.BEM()
+        self.weights = np.linalg.solve(self.B, self.c)
 
     def G(self, x, y):
         """Green function for 2D Helmholtz."""
@@ -32,7 +27,7 @@ class HelmholtzSystem:
         """Partial derivative of Green function wrt. y in second variable."""
         w, x = r_0
         y, z = r
-        # return self.k * sci_sp.hankel1(1, self.k * np.linalg.norm(x - y)) / 4.0j
+
         return 1.0j * self.k * (z - x) * sci_sp.hankel1(1, self.k * np.linalg.norm(r_0 - r)) / (4 * np.linalg.norm(r_0 - r))
 
     def D2G(self, r_0, r):
@@ -46,37 +41,46 @@ class HelmholtzSystem:
 
         return - (self.k * 1.0j / 8) * (s1 + s2)
 
-    def init_problem(self):
-        """Initialise the problem."""
+    def BEM(self):
+        """Initialise the BIE and solve using BEM."""
         bc = self.bcs
-        N = np.floor(10 * self.k)
-        # eps = 10**(-15)
+        N = self.N
 
-        # pick nodes on boundary based on wave number
+        # pick nodes on boundary based on wave number, evenly spaced
         nodes = np.arange(-1 + 1/N, 1, 2/N)
 
         # set up vector for matrix eqn. based on bcs
         if bc == "D":
             c = -np.ones(N, dtype=np.complex64)  # boundary condition u_i(x) = 1 on gamma
         elif bc == "N":
-            c = -1.0j * self.k * np.ones(N, dtype=np.complex64)
+            c = -1.0j * self.k * np.ones(N, dtype=np.complex64)  # boundary condition du_i/dy = ik on gamma
 
         A = np.zeros((N, N), dtype=np.complex64)
+
         if bc == "D":
-            # dirichlet: u = 0 on boundary
+            # dirichlet problem:
+            # 
+            # WIP
+
             for i in range(N):
+                # we require real and imaginary parts of the function as scipy.integrate.quad() supports real-valued functions only
                 def f_r(x): return (1.0j * self.k * self.G(np.array([0, nodes[i]]), np.array([0, x])) - self.DG(np.array([0, nodes[i]]), np.array([0, x]))).real
                 def f_i(x): return (1.0j * self.k * self.G(np.array([0, nodes[i]]), np.array([0, x])) - self.DG(np.array([0, nodes[i]]), np.array([0, x]))).imag
+
                 for j in range(N):
                     if i != j:
                         A[i, j] = sci_int.quad(f_r, nodes[j] - 1/N, nodes[j] + 1/N, limit=np.floor(40*self.k))[0] + 1.0j*sci_int.quad(f_i, nodes[j] - 1/N, nodes[j] + 1/N, limit=np.floor(40*self.k))[0]
                     else:
-                        # A[i, j] = sci_int.quad(f_r, nodes[j] - 1/N + eps, nodes[j] + 1/N + eps, limit=np.floor(40*self.k))[0] + 1.0j*sci_int.quad(f_i, nodes[j] - 1/N + eps, nodes[j] + 1/N + eps, limit=np.floor(40*self.k))[0]
+                        # TODO: find analytical solution for i = j to avoid integrating over singularity
                         A[i, j] = 0 + 0j
                 print(A[i, i])
             B = np.identity(N) / 2 - A
+
         elif bc == "N":
-            # neumann: du/dy = 0 on boundary
+            # neumann problem:
+            #
+            # WIP
+
             for i in range(N):
                 def f_r(x): return (self.k ** 2 * self.G(np.array([0, nodes[i]]), np.array([0, x])) - 1.0j * self.k * self.DG(np.array([0, x]), np.array([0, nodes[i]])) + self.D2G(np.array([0, nodes[i]]), np.array([0, x]))).real
                 def f_i(x): return (self.k ** 2 * self.G(np.array([0, nodes[i]]), np.array([0, x])) - 1.0j * self.k * self.DG(np.array([0, x]), np.array([0, nodes[i]])) + self.D2G(np.array([0, nodes[i]]), np.array([0, x]))).imag
@@ -84,46 +88,52 @@ class HelmholtzSystem:
                     if i != j:
                         A[i, j] = sci_int.quad(f_r, nodes[j] - 1/N, nodes[j] + 1/N, limit=np.floor(40*self.k))[0] + 1.0j*sci_int.quad(f_i, nodes[j] - 1/N, nodes[j] + 1/N, limit=np.floor(40*self.k))[0]
                     else:
-                        # A[i, j] = sci_int.quad(f_r, nodes[j] - 1/N + eps, nodes[j] + 1/N + eps, limit=np.floor(40*self.k))[0] + 1.0j*sci_int.quad(f_i, nodes[j] - 1/N + eps, nodes[j] + 1/N + eps, limit=np.floor(40*self.k))[0]
+                        # TODO: find analytical solution for i = j to avoid integrating over singularity
                         A[i, j] = 0 + 0j
                 print(A[i, i])
             B = 1.0j * self.k * np.identity(N) / 2 + A
-        return c, B  # type: ignore
+        return c, B
 
     def weight(self, x):
-        # use piecewise function for faster integrating to calculate u_s (O(n^2) as opposed to O(n^3))
-        N = np.floor(10*self.k)
+        """Define calculated piecewise weight function."""
+        N = self.N
         y = int((1 + x) // (2 / N))
         if y == N:
             y = int(N - 1)
-        return self.coeffs[y]
+        return self.weights[y]
 
     def u_scat(self, r):
-        N = np.floor(10*self.k)
-        antinodes = np.linspace(-1, 1, N+1)
+        """Calculate scattered wave at point r."""
+
+        # list of intersections between discretised plates
+        antinodes = np.linspace(-1, 1, self.N+1)
 
         sum = 0
 
-        for i in range(N):
-            def f_r(x): return ((self.DG(r, np.array([x, 0])) - 1.0j * self.k * self.G(r, np.array([x, 0]))) * self.coeffs[i]).real
-            def f_i(x): return ((self.DG(r, np.array([x, 0])) - 1.0j * self.k * self.G(r, np.array([x, 0]))) * self.coeffs[i]).imag
+        # integrating over each plate individually and taking the sum is slower in theory
+        # however integrating over the whole boundary yields inconsistencies due to the weight
+        # being discontinuous (convergence of quadrature is slow)
+        # this also yields a more accurate estimate
+
+        for i in range(self.N):
+            def f_r(x): return ((self.DG(r, np.array([x, 0])) - 1.0j * self.k * self.G(r, np.array([x, 0]))) * self.weights[i]).real
 
             sum += sci_int.quad(f_r, antinodes[i], antinodes[i+1], limit=100)[0]
 
-
-
         return sum
-        # return sci_int.quad(f_r, -1, 1, limit=100)[0] # + 1.0j * sci_int.quad(f_i, -1, 1)[0]
 
 
+#############
+## TESTING ##
+#############
+
+
+# wave number
 k = 5
 
 sys = HelmholtzSystem(k, "D")
 
-print(sys.G(np.array([0, 1]), np.array([1, 1])))
-
-print(sys.u_scat(np.array([0, 1])))
-
+# resolution of graph
 n = 200
 
 xvals = np.linspace(-3, 3, n)
@@ -131,10 +141,7 @@ yvals = np.linspace(-3, 3, n)
 
 x, y = np.meshgrid(xvals, yvals)
 
-
 stack = np.column_stack((x.flatten(), y.flatten()))
-
-print(stack)
 
 z_vals = np.ones(len(stack))
 
@@ -143,20 +150,16 @@ for i in range(len(z_vals)):
     if i % 100 == 0:
         print(f"{i}/{len(z_vals)}")
 
+# temporary measure to renormalise waves
 max = np.max(z_vals)
-
-# z_v = [z_vals[i] / max + np.exp(1.0j * k * stack[i][1]).real for i in range(len(z_vals))]
 
 z = np.reshape(z_vals, (-1, n))
 
-
-print(z.shape)
-
 fig, ax = plt.subplots(subplot_kw={"projection": "3d"})
 
-surf = ax.plot_surface(x, y, z, cmap=cm.coolwarm, linewidth=0, antialiased=False)  # type: ignore
+surf = ax.plot_surface(x, y, z, cmap=cm.coolwarm, linewidth=0, antialiased=False)
 
-ax.set_zlim(-5, 5)  # type: ignore
+ax.set_zlim(-5, 5)
 fig.colorbar(surf, shrink=0.5, aspect=5)
 
 plt.show()
